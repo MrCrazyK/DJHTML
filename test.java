0b553eefc6347670d7d6bf9243d2c442c14bdd60
MrCrazyK <31610791+MrCrazyK@users.noreply.github.com>
2022-07-15 10:14:02 +0800

    11

--- test.java
+++ test.java
@@ -1,6 +1,16 @@
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class test{
     public static void main(String[] args){
         // System.out.println("Hello,world!");
+        //开启socket 参数：端口号
         ServerSocket server = new ServerSocket(8080);
+
+        Socket socket = server.accept();
+        
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        String string = in.readUTF();//接收结果
     }
 }
